ca0a0a29e4a212bbdc8abb4d1ddf0928604f662d
drvanegas6 <89328823+drvanegas6@users.noreply.github.com>
2021-08-22 01:11:10 -0500

    Update api.py

--- api.py
+++ api.py
@@ -2,6 +2,12 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from methods import Token, Restricted
+import hashlib, base64
+import mysql.connector
+
+Conection_db = mysql.connector.connect( host='localhost', user= 'secret', passwd='noPow3r', port=8000, db='bootcamp_tht' )
+cur = Conection_db.cursor()
+cur.execute( "SELECT username, password, salt FROM users" )
 
 app = Flask(__name__)
 login = Token()
@@ -23,10 +29,15 @@ def url_health():
 # e.g. http://127.0.0.1:8000/login
 @app.route("/login", methods=['POST'])
 def url_login():
+
     username = request.form['username']
     password = request.form['password']
+    salt = request.form['salt']
+    saltpass = username+password+salt
+    
     res = {
-        "data": login.generate_token(username, password)
+        hashlib.sha256(saltpass.encode()).hexdigest()
+        "data": login.generate_token(salt, username, password)
     }
     return jsonify(res)
 
@@ -43,3 +54,5 @@ def url_protected():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
+    
+Conection_db.close()
